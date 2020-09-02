ac555c49476961c70d8224cef6d2d65bb6c88fb9
LeCaitlyn <69994571+LeCaitlyn@users.noreply.github.com>
2020-09-02 17:51:33 -0400

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,17 @@
+# Caitlyn Le
+# INFO 450
+# Week 2 - Homework 1
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+def squared_threes():
+    return_value = []
+    squares = [value ** 2 for value in list(range(3,102,3))]
+    return_value.append(squares)
+    return return_value
+
+if __name__ == "__main__":
+    for x in squared_threes():
+        print(x)
+
