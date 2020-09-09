a8bc755fb9e8f426eac80294a112efd9f8f3f419
Caitlyn <lece@mymail.vcu.edu>
2020-09-08 23:52:47 -0400

    Week 3 Homework

---
+++ check_users.py
@@ -0,0 +1,20 @@
+def check_users(current_users, new_users):
+    pass
+
+current_users = ['chris','haritha', 'sally', 'darnell', 'supErman']
+new_users =['george', 'ringo', 'SuPerMan', 'hannibal', 'Josh']
+
+new_list = []
+for current_user in current_users:
+    new_list.append(current_user.lower())
+
+for new_user in new_users:
+    if new_user.lower() not in new_list:
+        print(f"The username {new_user} is available!")
+else:
+    print("This username is already taken.")
+
+if __name__ == "__main__":
+ current_us = ['chris','haritha', 'sally', 'darnell', 'superman']
+ new_us = ['george', 'ringo', 'superman', 'hannibal']
+ check_users(current_us, new_us)
