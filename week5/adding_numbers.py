9f967bc8241f5e0d158db4d99be030e0799a8651
Caitlyn <lece@mymail.vcu.edu>
2020-09-23 20:32:33 -0400

    Week 5 Homework

---
+++ week5/adding_numbers.py
@@ -0,0 +1,20 @@
+import sys
+
+def add_them_all(filename):
+    sum = 0
+
+    with open(filename, 'r') as f:
+        contents = f.read()
+    number_list = contents.split('\n')
+    number_list.remove('')
+
+    for i in range(0, len(number_list)): 
+        number_list[i] = int(number_list[i])
+        sum = sum + number_list[i]
+
+    return sum
+
+if __name__ == "__main__":
+    fname = sys.argv[1]
+    print(f"Processing {fname}")
+    print(add_them_all(fname))
